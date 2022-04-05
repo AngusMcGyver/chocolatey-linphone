1d067fc537b75b0b298716aaf364bbb089caf3d2
Angus McGyver <angus.mcgyver@gmx.de>
2022-04-05 16:36:41 +0200

    update for version 4.4.0

---
+++ source/4.4.0/tools/ChocolateyInstall.ps1
@@ -0,0 +1,41 @@
+$ErrorActionPreference = 'Stop'
+
+$LinphoneActive = Get-Process Linphone -ErrorAction SilentlyContinue
+if($LinphoneActive -eq $null) {
+	# the silent installer won't start the uninstaller of the old version in silent mode, so do it manually...
+	$packageArgsUnInstall = @{
+	packageName            = $env:ChocolateyPackageName
+	fileType               = 'EXE'
+	silentArgs             = '/S'
+	validExitCodes         = @(0)
+	softwareName           = 'linphone'
+	}
+
+	# get uninstaller
+	[array]$key = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match $packageArgsUnInstall['softwareName'] } | Select-Object -Property DisplayName, UninstallString
+	if ($key.Count -eq 1) {
+	$key | % {
+		$packageArgsUnInstall['file'] = "$($_.UninstallString)"
+		Write-Verbose "uninstall old $($_.DisplayName) version: $($_.UninstallString) $packageArgsUnInstall['silentArgs']"
+		Uninstall-ChocolateyPackage @packageArgsUnInstall
+	}
+	} elseif ($key.Count -eq 0) {
+	Write-Verbose "skip uninstall of old $packageArgsUnInstall['softwareName'] version, no installation found"
+	}
+
+	# install
+	$packageArgsInstall = @{
+	packageName            = $env:ChocolateyPackageName
+	fileType               = 'EXE'
+	url                    = 'https://download.linphone.org/releases/windows/app/Linphone-4.4.0-win64.exe'
+	checksum               = 'cc6184863346cbea801b471338f699c2097bf775326ee2df1a4caab26e22454c'
+	checksumType           = 'sha256'
+	silentArgs             = '/S'
+	validExitCodes         = @(0)
+	softwareName           = 'linphone'
+	}
+
+	Install-ChocolateyPackage @packageArgsInstall
+} else {
+ 	Write-Error "A version of linphone is already running, please stop and try again"
+}
