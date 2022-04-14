c4608e487b01cf32bfc7b30fdb5157e3b0310393
Angus McGyver <angus.mcgyver@gmx.de>
2022-04-14 11:02:45 +0200

    remove checksum from installscript, cause there is a daily rebuild and reupload of the latest linphone version

--- source/4.3.2/tools/ChocolateyInstall.ps1
+++ source/4.3.2/tools/ChocolateyInstall.ps1
@@ -4,35 +4,33 @@ $LinphoneActive = Get-Process Linphone -ErrorAction SilentlyContinue
 if($LinphoneActive -eq $null) {
 	# the silent installer won't start the uninstaller of the old version in silent mode, so do it manually...
 	$packageArgsUnInstall = @{
-	packageName            = $env:ChocolateyPackageName
-	fileType               = 'EXE'
-	silentArgs             = '/S'
-	validExitCodes         = @(0)
-	softwareName           = 'linphone'
+		packageName            = $env:ChocolateyPackageName
+		fileType               = 'EXE'
+		silentArgs             = '/S'
+		validExitCodes         = @(0)
+		softwareName           = 'linphone'
 	}
 
 	# get uninstaller
 	[array]$key = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match $packageArgsUnInstall['softwareName'] } | Select-Object -Property DisplayName, UninstallString
 	if ($key.Count -eq 1) {
-	$key | % {
-		$packageArgsUnInstall['file'] = "$($_.UninstallString)"
-		Write-Verbose "uninstall old $($_.DisplayName) version: $($_.UninstallString) $packageArgsUnInstall['silentArgs']"
-		Uninstall-ChocolateyPackage @packageArgsUnInstall
-	}
+		$key | % {
+			$packageArgsUnInstall['file'] = "$($_.UninstallString)"
+			Write-Verbose "uninstall old $($_.DisplayName) version: $($_.UninstallString) $packageArgsUnInstall['silentArgs']"
+			Uninstall-ChocolateyPackage @packageArgsUnInstall
+		}
 	} elseif ($key.Count -eq 0) {
-	Write-Verbose "skip uninstall of old $packageArgsUnInstall['softwareName'] version, no installation found"
+		Write-Verbose "skip uninstall of old $packageArgsUnInstall['softwareName'] version, no installation found"
 	}
 
 	# install
 	$packageArgsInstall = @{
-	packageName            = $env:ChocolateyPackageName
-	fileType               = 'EXE'
-	url                    = 'https://download.linphone.org/releases/windows/app/Linphone-4.3.2-win32.exe'
-	checksum               = '1e207bb90b1990bf4fb6da170f044263edf8fe89baea25d64ba17d305d8115f5'
-	checksumType           = 'sha256'
-	silentArgs             = '/S'
-	validExitCodes         = @(0)
-	softwareName           = 'linphone'
+		packageName            = $env:ChocolateyPackageName
+		fileType               = 'EXE'
+		url                    = 'https://download.linphone.org/releases/windows/app/Linphone-4.3.2-win32.exe'
+		silentArgs             = '/S'
+		validExitCodes         = @(0)
+		softwareName           = 'linphone'
 	}
 
 	Install-ChocolateyPackage @packageArgsInstall
