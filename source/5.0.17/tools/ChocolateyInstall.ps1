869612ff7c345acaf3628cd413f7bb7f651bbfe3
Angus McGyver <angus.mcgyver@gmx.de>
2023-06-07 11:14:31 +0200

    add version 5.0.17

---
+++ source/5.0.17/tools/ChocolateyInstall.ps1
@@ -0,0 +1,24 @@
+. "$PSScriptRoot/linphone-helpers.ps1"
+
+$ErrorActionPreference = 'Stop'
+
+$LinphoneActive = Get-Process Linphone -ErrorAction SilentlyContinue
+if($LinphoneActive -eq $null) {
+
+	# the silent installer won't start the uninstaller of the old version in silent mode, so do it manually...
+	linphone_uninstall($true)
+
+	# install
+	$packageArgsInstall = @{
+		packageName            = $env:ChocolateyPackageName
+		fileType               = 'EXE'
+		url                    = 'https://download.linphone.org/releases/windows/app/Linphone-5.0.17-win64.exe'
+		silentArgs             = '/S'
+		validExitCodes         = @(0)
+		softwareName           = 'linphone'
+	}
+
+	Install-ChocolateyPackage @packageArgsInstall
+} else {
+ 	Write-Error "A version of $($packageArgsInstall['softwareName']) is already running, please stop and try again"
+}
